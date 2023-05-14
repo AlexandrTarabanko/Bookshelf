a16fbad984a2e4fbfbc70a868557f7fbbe7ca9b0
Olha Tverdokhlib <balabolya@gmail.com>
2023-05-14 19:59:29 +0200

    slider done

--- src/js/api/fonds-info.js
+++ src/js/api/fonds-info.js
@@ -5,7 +5,11 @@ const fondsInfo = [
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
     img: {
       imageUrl: new URL(
-        '../../images/png-icons/fonds/children.png?as=webp&width=250',
+        '../../images/png-icons/fonds/children.png?as=webp&width=129',
+        import.meta.url
+      ),
+      retinaImageUrl: new URL(
+        '../../images/png-icons/fonds/children@2x.png?as=webp&width=258',
         import.meta.url
       ),
     },
@@ -15,17 +19,11 @@ const fondsInfo = [
     url: 'https://www.projecthope.org/country/ukraine/',
     img: {
       imageUrl: new URL(
-        '../../images/png-icons/fonds/hope.png?as=webp&width=250',
+        '../../images/png-icons/fonds/hope.png?as=webp&width=62',
         import.meta.url
       ),
-    },
-  },
-  {
-    title: 'UNITED24',
-    url: 'https://u24.gov.ua/uk',
-    img: {
-      imageUrl: new URL(
-        '../../images/png-icons/fonds/united.png?as=webp&width=250',
+      retinaImageUrl: new URL(
+        '../../images/png-icons/fonds/hope@2x.png?as=webp&width=124',
         import.meta.url
       ),
     },
@@ -35,57 +33,101 @@ const fondsInfo = [
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
     img: {
       imageUrl: new URL(
-        '../../images/png-icons/fonds/med-corps.png?as=webp&width=250',
+        '../../images/png-icons/fonds/med-corps.png?as=webp&width=103',
+        import.meta.url
+      ),
+      retinaImageUrl: new URL(
+        '../../images/png-icons/fonds/med-corps@2x.png?as=webp&width=206',
         import.meta.url
       ),
     },
   },
-  {
-    title: 'Medicins Sans Frontieres',
-    url: 'https://www.msf.org/ukraine',
-    img: {
-      imageUrl: new URL(
-        '../../images/png-icons/fonds/frontieres.png?as=webp&width=250',
-        import.meta.url
-      ),
-    },
-  },
+
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
     img: {
       imageUrl: new URL(
-        '../../images/png-icons/fonds/razom.png?as=webp&width=250',
+        '../../images/png-icons/fonds/razom.png?as=webp&width=82',
+        import.meta.url
+      ),
+      retinaImageUrl: new URL(
+        '../../images/png-icons/fonds/razom@2x.png?as=webp&width=164',
         import.meta.url
       ),
     },
   },
+
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
     img: {
       imageUrl: new URL(
-        '../../images/png-icons/fonds/hunger.png?as=webp&width=250',
+        '../../images/png-icons/fonds/hunger.png?as=webp&width=55',
+        import.meta.url
+      ),
+      retinaImageUrl: new URL(
+        '../../images/png-icons/fonds/hunger@2x.png?as=webp&width=110',
         import.meta.url
       ),
     },
   },
-  {
-    title: 'World vision',
-    url: 'https://www.wvi.org/emergencies/ukraine',
-    img: {
-      imageUrl: new URL(
-        '../../images/png-icons/fonds/vision.png?as=webp&width=250',
-        import.meta.url
-      ),
-    },
-  },
+
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
     img: {
       imageUrl: new URL(
-        '../../images/png-icons/fonds/prytula.png?as=webp&width=250',
+        '../../images/png-icons/fonds/prytula.png?as=webp&width=115',
+        import.meta.url
+      ),
+      retinaImageUrl: new URL(
+        '../../images/png-icons/fonds/prytula@2x.png?as=webp&width=230',
+        import.meta.url
+      ),
+    },
+  },
+
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: {
+      imageUrl: new URL(
+        '../../images/png-icons/fonds/united.png?as=webp&width=129',
+        import.meta.url
+      ),
+      retinaImageUrl: new URL(
+        '../../images/png-icons/fonds/united@2x.png?as=webp&width=258',
+        import.meta.url
+      ),
+    },
+  },
+
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: {
+      imageUrl: new URL(
+        '../../images/png-icons/fonds/frontieres.png?as=webp&width=114',
+        import.meta.url
+      ),
+      retinaImageUrl: new URL(
+        '../../images/png-icons/fonds/frontieres@2x.png?as=webp&width=228',
+        import.meta.url
+      ),
+    },
+  },
+
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: {
+      imageUrl: new URL(
+        '../../images/png-icons/fonds/vision.png?as=webp&width=60',
+        import.meta.url
+      ),
+      retinaImageUrl: new URL(
+        '../../images/png-icons/fonds/vision@2x.png?as=webp&width=120',
         import.meta.url
       ),
     },
--- src/js/slider-set.js
+++ src/js/slider-set.js
@@ -1,4 +1,3 @@
-
 import Swiper, { Navigation } from 'swiper';
 import 'swiper/swiper-bundle.css';
 import fondsInfo from './api/fonds-info.js';
@@ -6,50 +5,68 @@ import fondsInfo from './api/fonds-info.js';
 Swiper.use([Navigation]);
 
 function generateSlides() {
-  // const slidesPerView = 3; // Количество слайдов, видимых одновременно
-  // const initialSlides = fondsInfo.slice(0, 8);
+  const isMobile = window.innerWidth < 768;
+  const slidesPerView = isMobile ? 4 : 6;
+  let allSlides;
 
- 
+  if (fondsInfo.length > slidesPerView * 2) {
+    console.log(fondsInfo.length);
+    allSlides = fondsInfo;
+  } else {
+    allSlides = [...fondsInfo, ...fondsInfo];
+  }
+  console.log(allSlides);
 
-  const swiper = new Swiper('.swiper-container', {
+  let swiper;
+
+  function updateSlider() {
+    if (swiper) {
+      swiper.update();
+    }
+  }
+
+  function handleResize() {
+    updateSlider();
+  }
+
+  const swiperOptions = {
     direction: 'vertical',
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 25,
+    slidesPerView: slidesPerView,
+    spaceBetween: 20,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    // freeMode: true
-  });
+  };
 
-   fondsInfo.forEach((slide, index) => {
-     const slideHTML = `
+  swiper = new Swiper('.swiper-container', swiperOptions);
+
+  allSlides.forEach((slide, index) => {
+    const originalIndex = index % fondsInfo.length;
+    const slideHTML = `
       <div class="swiper-slide">
-        <div class="slide-number">${index + 1}</div>
-        <a href="${slide.url}">
-          <img src="${slide.img.imageUrl.href}" alt="${slide.title}">
-        </a>
+        <div class="slide__content">
+          <div class="slide__number">${(originalIndex + 1)
+            .toString()
+            .padStart(2, '0')}</div>
+          <a href="${slide.url}" class="slider__link" target="blank">
+            <img srcset="${slide.img.imageUrl.href}, ${slide.img.retinaImageUrl.href} 2x" 
+            src="${slide.img.imageUrl.href}
+            alt="${slide.title}" 
+            class="slider__png">
+          </a>
+        </div>
       </div>
     `;
-     document
-       .querySelector('.swiper-wrapper')
-       .insertAdjacentHTML('beforeend', slideHTML);
-   });
-  
-  // const nextButton = document.querySelector('.swiper-button-next');
-  // const prevButton = document.querySelector('.swiper-button-prev');
+    document
+      .querySelector('.swiper-wrapper')
+      .insertAdjacentHTML('beforeend', slideHTML);
+  });
 
-  // nextButton.addEventListener('click', () => {
-  //   swiper.slideNext();
-  // });
+  updateSlider();
 
-  // prevButton.addEventListener('click', () => {
-  //   swiper.slidePrev();
-  // });
-
-  swiper.update();
+  window.addEventListener('resize', handleResize);
 }
-// generateSlides();
 
 document.addEventListener('DOMContentLoaded', generateSlides);
