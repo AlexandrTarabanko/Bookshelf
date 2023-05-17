3227d037e40972ef18a1a4320f1a6114eb0922f3
Yura Basiuk <yurik.kamenev@gmail.com>
2023-05-17 22:34:48 +0300

    fix
    mobile menu, header, dark theme

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -23,6 +23,6 @@
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    // bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
