0b5f532b5b4c98e4b5dd4b7cdfb7b7964416cbc6
AlexandrTarabanko <azerov.commerce@gmail.com>
2023-05-18 09:44:24 +0000

    Deploying to gh-pages from @ AlexandrTarabanko/Bookshelf@c080662d7af19d19b32d41660199afd50adac236 🚀

---
+++ shop-list.b6a8b882.js
@@ -0,0 +1,2 @@
+const $menuBtn=document.getElementById("btn-hamburger");function animatedMenu(n){n.classList.toggle("animeOpenClose")}$menuBtn.onclick=function(n){animatedMenu(this),n.preventDefault()};
+//# sourceMappingURL=shop-list.b6a8b882.js.map
